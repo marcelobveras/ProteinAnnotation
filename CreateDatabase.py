f48b32549b048656f31dd9a3c2b6aedd8d8e0f61
Marcelo Veras <marcelobveras@gmail.com>
2018-04-11 16:44:55 -0300

    Database Creator to train automatic annotation

--- CreateDatabase.py
+++ CreateDatabase.py
@@ -1,7 +1,7 @@
 import numpy as np
+import pandas as pd
 
-file = open("C:\\Users\\marce\\Desktop\\uniprot\\mammals001.dat")
-
+file = open("C:\\Users\\marce\\Desktop\\uniprot\\mammals.dat")
 
 #Primeira Leitura
 #Criação dos conjuntos de palavras
@@ -10,7 +10,7 @@ Codes = {'OC', 'DR', 'ID', 'KW'}
 OCset = set()
 DRset = set()
 KWset = set()
-idCount = 0
+IDset = set()
 
 for row in file:
     #print(row[0:2])
@@ -21,7 +21,7 @@ for row in file:
         line[-1] =  line[-1][:-1]
         words = line[1].split(';')
         if(line[0] == "ID"):
-            idCount = idCount+1
+            IDset.add(words[0].lstrip())
         if(line[0] == "OC"):
             for word in words:
                 OCset.add(word.lstrip())
@@ -32,7 +32,38 @@ for row in file:
             DRstring = words[0].lstrip()+':'+words[1].lstrip()
             DRset.add(DRstring)
 
-#Creando matrizes booleanas
-OCMatrix = np.zeros((idCount,len(OCset)), dtype=bool);
-DRMatrix = np.zeros((idCount,len(KWset)), dtype=bool);
-KWMatrix = np.zeros((idCount,len(DRset)), dtype=bool);
+#Incializando matrizes booleanas e dataframes correspondentes
+OCMatrix = np.zeros((len(IDset),len(OCset)), dtype=int)
+DRMatrix = np.zeros((len(IDset),len(DRset)), dtype=int)
+KWMatrix = np.zeros((len(IDset),len(KWset)), dtype=int)
+
+OCDataFrame = pd.DataFrame(data=OCMatrix, index=IDset, columns=OCset)
+DRDataFrame = pd.DataFrame(data=DRMatrix, index=IDset, columns=DRset)
+KWDataFrame = pd.DataFrame(data=KWMatrix, index=IDset, columns=KWset)
+
+
+#Segunda leitura do arquivo (Criação do dataset)
+file.seek(0)
+for row in file:
+    if(row[0:2] in Codes):
+        line = row.split("   ")
+        line[-1] = line[-1].strip()
+        line = [empEle for empEle in line if empEle]
+        line[-1] =  line[-1][:-1]
+        words = line[1].split(';')
+        if(line[0] == "ID"):
+            idname = words[0].lstrip()
+        if(line[0] == "OC"):
+            for word in words:
+                ocname = word.lstrip()
+                OCDataFrame[ocname][idname] = 1
+        if(line[0] == "KW"):
+            for word in words:
+                kwname = word.lstrip()
+                KWDataFrame[kwname][idname] = 1
+        if(line[0] == "DR"):
+            DRstring = words[0].lstrip()+':'+words[1].lstrip()
+            DRDataFrame[DRstring][idname] = 1
+DRDataFrame.to_csv("DRdata", sep=';')
+OCDataFrame.to_csv("OCData", sep=';')
+KWDataFrame.to_csv("KWData", sep=';')
