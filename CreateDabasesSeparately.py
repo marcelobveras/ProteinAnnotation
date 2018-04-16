f893475d86dfc673c1e6b8ff14e073ffab32adff
Marcelo Veras <marcelobveras@gmail.com>
2018-04-16 10:38:51 -0300

    DR Embl (precisa de uma generalização para as linhas DR)

--- CreateDabasesSeparately.py
+++ CreateDabasesSeparately.py
@@ -1,5 +1,8 @@
 import numpy as np
 import pandas as pd
+from collections import Counter
+
+trsld = 0.0001
 
 file = open("C:\\Users\\marce\\Desktop\\uniprot\\mammals.dat")
 
@@ -67,7 +70,7 @@ for row in file:
             for word in words:
                 KWset.add(word.lstrip())
 
-KWMatrix = np.zeros((len(IDset),len(OCset)), dtype='uint8')
+KWMatrix = np.zeros((len(IDset),len(KWset)), dtype='uint8')
 KWDataFrame = pd.DataFrame(data=KWMatrix, index=IDset, columns=KWset)
 
 file.seek(0)
@@ -85,4 +88,50 @@ for row in file:
                 kwname = word.lstrip()
                 KWDataFrame[kwname][idname] = 1
 
-OCDataFrame.to_csv("KWData.dat", sep=';')
+KWDataFrame.to_csv("KWData.dat", sep=';')
+
+##--
+#DR Embl Dataset
+file.seek(0)
+DREMBLdict = {}
+DREMBLset = set()
+
+for row in file:
+    if(row[0:2] in ('DR')):
+        line = row.split("   ")
+        line[-1] = line[-1].strip()
+        line = [empEle for empEle in line if empEle]
+        line[-1] =  line[-1][:-1]
+        words = line[1].split(';')
+        if(line[0] == "DR"):
+            if(words[0] == "EMBL"):
+                keyDR = words[1].lstrip();
+                if( keyDR in DREMBLdict):
+                    DREMBLdict[keyDR] = DREMBLdict[keyDR]+1
+                else:
+                    DREMBLdict[keyDR] = 1
+nTot = len(DREMBLdict)
+for DRkey in DREMBLdict:
+    if (DREMBLdict[DRkey]/nTot >= trsld):
+        DREMBLset.add(DRkey)
+
+DREMBLMatrix = np.zeros((len(IDset),len(DREMBLset)), dtype='uint8')
+DREMBLDataFrame = pd.DataFrame(data=DREMBLMatrix, index=IDset, columns=DREMBLset)
+
+file.seek(0)
+for row in file:
+    if(row[0:2] in ('ID','DR')):
+        line = row.split("   ")
+        line[-1] = line[-1].strip()
+        line = [empEle for empEle in line if empEle]
+        line[-1] =  line[-1][:-1]
+        words = line[1].split(';')
+        if(line[0] == "ID"):
+            idname = words[0].lstrip()
+        if(line[0] == "DR"):
+            if(words[0] == "EMBL"):
+                if (words[1] in DREMBLset):
+                    DRstring = words[1].lstrip()
+                    DREMBLDataFrame[DRstring][idname] = 1
+
+DREMBLDataFrame.to_csv("DREMBLData.dat", sep=';')
